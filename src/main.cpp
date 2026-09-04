cda59bc7d2db55c8b4a0e642d43ac82ab5813a8d
kuromamekuromame88 <tei8sia@outlook.jp>
2026-09-04 18:18:45 +0900

    送信は完成？

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,23 @@
+//stty -F /dev/ttyACM0 115200 raw -echo && cat /dev/ttyACM0
+
+#include <Arduino.h>
 #include "CANf303.hpp"
 
 STM32CAN can;
 
 volatile uint32_t receivedCount = 0;
 
-void onCanReceive(twai_message_t msg)
-{
-  
-  /*
+twai_message_t receive;
+void onCanReceive(twai_message_t msg){
+
     receivedCount++;
 
+    //receive.identifier = msg.identifier;
+    //receive.extd = msg.extd;
+    //receive.rtr = msg.rtr;
+    //receive.data_length_code = msg.data_length_code;
+    //memcpy(receive.data, msg.data, msg.data_length_code);
+    /*
     Serial.println("=== can RX ===");
     Serial.printf("ID   : 0x%lX\n", msg.identifier);
     Serial.printf("EXTD : %lu\n", msg.extd);
@@ -21,29 +29,29 @@ void onCanReceive(twai_message_t msg)
     for (uint8_t i = 0; i < msg.data_length_code; i++) {
         Serial.printf("%02X ", msg.data[i]);
     }
+    Serial.println();    
+    */
 
-    Serial.println();*/
+    
 }
 
-void setup()
-{
+void setup(){
     Serial.begin(115200);
     delay(1000);
 
     Serial.println("can loopback test");
 
     if (!can.begin(500000, PA12_PA11)) {
-        Serial.println("失敗can initialization failed");
+        Serial.println("CANの初期化失敗");
         while (1);
     }
 
     can.onReceive(onCanReceive);
 
-    Serial.println("成功can initialized");
+    Serial.println("CANの初期化成功");
 }
 
 void loop() {
-    Serial.println("before send");
 
     twai_message_t msg = {};
     msg.extd = STANDARD_FORMAT;
@@ -57,9 +65,24 @@ void loop() {
 
     Serial.println("calling send");
     bool result = can.send(msg);
-    Serial.println("send returned");
-
     Serial.printf("send result = %d\n", result);
 
     delay(1000);
+
+    /*
+    Serial.printf(
+        "TSR=0x%08lX IER=0x%08lX MSR=0x%08lX\n",
+        CAN1->TSR,
+        CAN1->IER,
+        CAN1->MSR
+    );
+    Serial.printf(
+        "TMEIE=%d\n",
+        (CAN1->IER & CAN_IER_TMEIE) != 0
+    );*/
+    //Serial.printf("txIrqCount = %lu\n", txIrqCount);
+    //Serial.printf("received count: %d\n", receivedCount);
+    //Serial.printf("can.available: \n", can.available());
+    //Serial.print("received ID:");
+    //Serial.println(receive.identifier);
 }
